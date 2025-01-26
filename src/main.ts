775b8b9683191f39314035007c7647f70fb2a8e5
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-01-25 22:25:24 -0300

    21 - Twenty-first commit: Removed PrimeNG and its configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';import * as tf from '@tensorflow/tfjs';
-import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import Aura from '@primeng/themes/aura';
 
 async function initializeTensorFlow() {
   try {
@@ -18,14 +14,5 @@ async function initializeTensorFlow() {
 
 initializeTensorFlow();
 
-bootstrapApplication(AppComponent,{
-  providers: [
-    provideRouter([]),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura,
-      },
-    }),
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
