15e21fb6bb14c102258dfa860a3de2a2f009e293
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-01-30 23:32:17 -0300

    53 - Fifty-third commit: Deleted unnecessary files and made final adjustments to the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { CameraComponent } from './shared/camera/camera.component';
 import { WebcamDirective } from './shared/webcam/webcam.directive';
 import { PoseDetectionComponent } from './components/pose-detection/pose-detection.component';
 import { HandDetectionComponent } from './components/hand-detection/hand-detection.component';
@@ -11,7 +10,7 @@ import { FaceDetectionComponent } from './components/face-detection/face-detecti
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, CameraComponent, WebcamDirective, HandDetectionComponent, PoseDetectionComponent, ObjectDetectionComponent, FaceDetectionComponent],
+  imports: [CommonModule, RouterOutlet, WebcamDirective, HandDetectionComponent, PoseDetectionComponent, ObjectDetectionComponent, FaceDetectionComponent],
   templateUrl: './app.component.html',
   styleUrls: ['/app.component.css']
 })
